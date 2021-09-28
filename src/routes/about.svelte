0d95ecc58ebaba45610781f4715a1d4ae7e7edd0
Evan Walters <evan@evanw.uk>
2021-09-28 22:54:28 +0100

    fix about formatting

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -5,6 +5,5 @@
 <h1 class="text-4xl text-center my-8 uppercase">About</h1>
 <li class="text-center text-xl">Built as a learning tool for SvelteKit</li>
 <li class="text-center text-xl">
-	API at <a class="text-blue-900 underline" href="/api/pokemon">/api/pokemon</a> and
-	<a class="text-blue-900 underline" href="/api/pokemon/id">/api/pokemon/id</a>
+	API at <code>/api/pokemon/</code> and <code>/api/pokemon/[id]</code>
 </li>
