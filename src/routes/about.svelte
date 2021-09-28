9d957a4a858d11b201ea0ed96f2b4604c796bcfe
Evan Walters <evan@evanw.uk>
2021-09-28 22:01:35 +0100

    init

---
+++ src/routes/about.svelte
@@ -0,0 +1,10 @@
+<svelte:head>
+	<title>SKP | About</title>
+</svelte:head>
+
+<h1 class="text-4xl text-center my-8 uppercase">About</h1>
+<li class="text-center text-xl">Built as a learning tool for SvelteKit</li>
+<li class="text-center text-xl">
+	API at <a class="text-blue-900 underline" href="/api/pokemon">/api/pokemon</a> and
+	<a class="text-blue-900 underline" href="/api/pokemon/id">/api/pokemon/id</a>
+</li>
